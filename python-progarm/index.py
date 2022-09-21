5e3de786d62ea0de490728bc1a0b7e03ed6d4ef4
code-nick <97045262+code-nick@users.noreply.github.com>
2022-09-22 00:53:46 +0530

    Add files via upload

---
+++ python-progarm/index.py
@@ -0,0 +1,129 @@
+              
+              # ***********JAy Mata Di******************         
+
+# python code Hello word 
+#print("Jay hanuman ji")
+
+# +++++++++python program to declare variable+++++++++++++++ 
+
+#mynumber = 3
+#print(mynumber)
+
+#mynumber2 = 4.5
+#print(mynumber2)
+
+#mynumber3 = "my name is vicky verma"
+#print(mynumber3)
+
+
+#++++++++++++++++++++++program python to illustrate a list++++++++++++++++++++++++++++
+ # create a empty list
+#nums = []
+#nums.append(21)
+#nums.append(40.5)
+#nums.append("string")
+#print(nums)
+
+
+#++++++++++++ Python program to illustrate a Dictionary++++++++++++++++++++++++++
+
+# creates a empty list
+
+#dict = []
+#dict = {1: 'geeks' , 2: 'vicky', 3:'kush'}
+#print(dict)
+
+
+#+++++++++++++++++++++python program to illustrate a tuple++++++++++++++++++++++++
+# creates a tuple which is immutable
+
+#tup = ('vicky','for', 'geeks')
+#print(tup)
+
+#+++++++++++++++++++python program to illustrate a set ++++++++++++++++++++++++++
+
+# define a set and its elements
+
+#myset = set(["vicky" , "vishal" ,"Golu"])
+#print(myset)
+
+#+++++++++++++++++++python program to illustrate ++++++++++++++++++++++++++
+#getting input from user
+#name = input("Enter your name:")
+#user entered the name 'Vicky'
+#print("Hello",name)
+
+#+++++++++++++++++++++ Python3 program to get input from user++++++++++++++
+# accepting integer from the user
+# the return type of input() function is string ,
+# so we need to convert the input to integer
+
+#num1 = int(input("enter Your Num1:"))
+#num2 = int(input("Enter Your Num2:"))
+
+#num3 = num1* num2
+
+#print( "Prodect is:",num3)
+
+# ++++++++++++++Python program to illustrate++++++++++++++++
+# selection statement
+
+#num1 = 34
+#if(num1>12):
+
+#  print("Num1 is good")
+#elif(num1>35):
+# #  print("Num2 is not gooooo....")
+#else:
+#  # print("Num2 is great")
+
+
+#++++++++++++++ Python program to illustrate++++++++++++++++
+# functions
+
+#def hello():
+#  # print("Repeat")
+# #  print("Repeat again")
+#hello()
+  
+# calling function
+#hello()
+
+# +++++++++++++++Python program to illustrate+++++++++++++++++++
+# function with main
+#from unittest import result
+
+
+#def getinterger():
+   # result = int(input("Enter Your Integer"))
+    ###  output  = getinteger():
+#print(output)
+
+      #  if__name__ =="__main---":
+       # main()
+
+
+i#mport sys
+
+
+#N = int(input()).strip()
+
+#if N % 2 != 0:
+ #   print ("Weird")
+#else:
+ #   if N >= 2 and N <= 5:
+  #      print ("Not Weird")
+   # elif N >= 6 and N <= 20:
+     #   print ("Weird")
+    #elif N > 20:
+      #  print ("Not Weird")
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
---
+++ python-progarm/index2.py
@@ -0,0 +1,7 @@
+Name = input("Enter Your Name:")
+
+if(Name == 'admin'):
+
+    print("Welcome Admin")
+else:
+    print("Involid Admin  Name")

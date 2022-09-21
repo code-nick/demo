f2ad04ceffaa937706eab694109f28fa763e2666
code-nick <97045262+code-nick@users.noreply.github.com>
2022-09-22 00:47:52 +0530

    Add files via upload

---
+++ input-button/javascript.js
@@ -0,0 +1,15 @@
+function myFunctoin(){
+    var x = document.getElementById("myinput");
+    var y = document.getElementById("hide1");
+    var z = document.getElementById("hide2");
+    if(x.type === 'password'){
+        x.type = "text";
+        y.style.display = "block";
+        z.style.display = "none";
+    }
+    else{
+        x.type = "password";
+        y.style.display = "none";
+        z.style.display = "block";
+    }
+}
